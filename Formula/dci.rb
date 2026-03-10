4e480e3ba9d4e311f910b9361d3bedffd346f2a9
apgiorgi <apgiorgi@gmail.com>
2026-03-10 16:56:53 +0100

    chore: Update Homebrew formula and Scoop manifest for v0.2.0

--- Formula/dci.rb
+++ Formula/dci.rb
@@ -1,25 +1,25 @@
 class Dci < Formula
   desc "DoiT Cloud Intelligence CLI"
   homepage "https://github.com/doitintl/dci-cli"
-  version "0.1.0"
+  version "0.2.0"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/doitintl/dci-cli/releases/download/v0.1.0/dci_0.1.0_darwin_arm64.tar.gz"
-      sha256 "961d1700a447406380f471e2729cf7c0cb816a09cf43c146d30b81822a9f3d14"
+      url "https://github.com/doitintl/dci-cli/releases/download/v0.2.0/dci_0.2.0_darwin_arm64.tar.gz"
+      sha256 "6064734849b4a42aace39e6c307a414d985d6838b6a24e1b9d37dfecdf97a783"
     else
-      url "https://github.com/doitintl/dci-cli/releases/download/v0.1.0/dci_0.1.0_darwin_amd64.tar.gz"
-      sha256 "5b45023dbb824be5d5c1b7eb37c4c0cc16bdc66adbdf82e9f30f0cf4c0a3d141"
+      url "https://github.com/doitintl/dci-cli/releases/download/v0.2.0/dci_0.2.0_darwin_amd64.tar.gz"
+      sha256 "0dafb3977ab02f0bf0db2e0caf61edb747752012cb01f4c3d4f1a79283e0dcbe"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/doitintl/dci-cli/releases/download/v0.1.0/dci_0.1.0_linux_arm64.tar.gz"
-      sha256 "df89d819fbe0e16194847600df125fce53d194ee9edeee9c1e3d0450aab41668"
+      url "https://github.com/doitintl/dci-cli/releases/download/v0.2.0/dci_0.2.0_linux_arm64.tar.gz"
+      sha256 "cdccc3ade925f12ef2fc5911ccef1e87f8146b196a5cdf2d395cf3bb6bfb786a"
     else
-      url "https://github.com/doitintl/dci-cli/releases/download/v0.1.0/dci_0.1.0_linux_amd64.tar.gz"
-      sha256 "fe924ce44ae17309281f8ef1cc69dcfd3005ff6d515399d514f55fb72cfe4f51"
+      url "https://github.com/doitintl/dci-cli/releases/download/v0.2.0/dci_0.2.0_linux_amd64.tar.gz"
+      sha256 "2d061c88524fbd7b8e018815aee60bdfb7c7cac2bdf4849f81a0bc65fe0ff8db"
     end
   end
 
